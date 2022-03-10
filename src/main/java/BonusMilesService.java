f34ff748535bddae962b71cc1a42cec6727491e4
AleksandrAstahov <spirtt1985@yandex.ru>
2022-03-10 14:23:12 +0300

    Initial commit

---
+++ src/main/java/BonusMilesService.java
@@ -0,0 +1,9 @@
+public class BonusMilesService {
+    public int calculate (int cost){
+        int mile = 20;
+        int result = cost / mile;
+//        System.out.println("Вы получите " + result + " миль");
+
+        return result;
+    }
+}
